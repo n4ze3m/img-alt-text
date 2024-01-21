394646e6efb9da6077104eafb9fa8210a5993dee
n4ze3m <n4ze3m@gmail.com>
2024-01-21 23:57:51 +0530

    Refactor AI utility functions and add support for text rewriting

--- app/utils/ai.ts
+++ app/utils/ai.ts
@@ -1,15 +1,31 @@
 import { ChatFireworks } from "@langchain/community/chat_models/fireworks";
 import { HumanMessage } from "@langchain/core/messages";
 import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { PromptTemplate } from "@langchain/core/prompts";
 import { StringOutputParser } from "@langchain/core/output_parsers";
 
 const outputParser = new StringOutputParser();
 const ALT_TEXT_PROMPT = `Your task is to describe the following image for its alt tag text generation. It must be meaningful, providing full details about the image and be SEO-friendly. Please respond with the alt text only, without any additional information. The maximum length is max 200 characters; do not exceed it. Do not include anyother information in your response.
 
-Alt Text in {style}:`;
+Alt Text:`;
+
+const REWRITE_PROMPT = `You are a helpful assistant that can rewrite following text in a specific style. Do not include any other information in your response.
+
+Text: {text}
+
+Style: {style}
+
+
+Rewritten Text:
+`;
 
 const llavaVisionModel = new ChatFireworks({
   modelName: "accounts/fireworks/models/firellava-13b",
+  streaming: false,
+});
+
+const mixtralModel = new ChatFireworks({
+  modelName: "accounts/fireworks/models/mixtral-8x7b-instruct",
   streaming: false,
 });
 
@@ -39,9 +55,17 @@ export const generateAltText = async (
 
     const llavaPrompt = ChatPromptTemplate.fromMessages([llavaHuman]);
     const llavaChain = llavaPrompt.pipe(llavaVisionModel).pipe(outputParser);
-    const result = await llavaChain.invoke({
-      style: rewrite_format,
-    });
+    const result = await llavaChain.invoke({});
+
+    if (rewrite_format !== "formal") {
+      const rewritePrompt = PromptTemplate.fromTemplate(REWRITE_PROMPT);
+      const rewriteChain = rewritePrompt.pipe(mixtralModel).pipe(outputParser);
+      const rewriteResult = await rewriteChain.invoke({
+        text: result,
+        style: rewrite_format,
+      });
+      return rewriteResult;
+    }
 
     return result;
   } catch (error) {
